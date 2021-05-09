db2908820d9e96337b5fd233a1d71503ea56c8a8
unknown <mohsinraza1a@gmail.com>
2021-05-09 18:18:07 +0500

    Initial commit

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ball : MonoBehaviour {
+
+	int score;
+	public GameObject Splash_Prefab;
+	public float force;
+	public Text score_txt;
+	public GameObject GameMenu;
+
+
+
+
+	void OnCollisionEnter(Collision Col) {
+
+		if (Col.gameObject.tag == "h_piece" ) {
+
+
+
+			gameObject.GetComponent<AudioSource> ().Play ();
+			gameObject.GetComponent<Rigidbody> ().AddForce (new Vector3 (0f, 1f, 0f) * Time.deltaTime * force);
+			GameObject splash = Instantiate (Splash_Prefab);
+			Vector3 pos = transform.position;
+			pos.y = pos.y - 0.2f;
+
+			splash.transform.SetParent (GameObject.Find ("helix").transform);
+		}
+
+
+		else if (Col.gameObject.tag == "gameover") {
+
+			GameMenu.SetActive (true);
+		}
+	}
+
+	 
+
+
+
+		// function of score
+
+		void OnTriggerEnter(Collider c){
+
+		 if (c.gameObject.tag == "score_tag") {
+
+			score++;
+			score_txt.text = score + "";
+
+		}
+
+	}
+	void Update(){
+
+		if (transform.position.y < Camera.main.transform.position.y) {
+
+			Vector3 pos = Camera.main.transform.position;
+			pos.y = transform.position.y + 2.3f;
+			Camera.main.transform.position = pos;
+		}
+
+
+		}
+
+
+
+}
---
+++ Assets/Scripts/rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+	public float angle;
+
+	// Use this for initialization
+	void OnMouseDrag () {
+
+		float x = Input.GetAxis ("Mouse X");
+		transform.RotateAround (transform.position, new Vector3 (0, 1, 0) * Time.deltaTime *-1* x, angle);
+		
+	}
+
+
+}
